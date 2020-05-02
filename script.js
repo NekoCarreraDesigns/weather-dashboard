a4c4349506b4fd3ecaa5ea7a8ff84de2e9410773
NekoCarreraDesigns <denvermaas@gmail.com>
2020-05-02 01:07:58 -0600

    still trying to make the weather app work

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 var APIKey = "a8873d0d448c9dc93f60cba6ef75cb3e";
-var queryURL = "api.openweathermap.org/" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=" + APIKey;
 
 var resultsDiv = $("<div>");
 var fiveDayDiv = $("<div>");
 var cityh1El = $("<h1>");
-var prevEntryDiv = $("<aside>")
-$("#search").on("click", function (event) {
-    if (response === previous) {
-        prevEntryDiv.append.text(response.name)
-    }
-})
+var previousresultsDiv = $("<aside>");
+
+
+
+
+
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
+    console.log(queryURL)
     console.log(response)
-    // ("cityh1El").text(response.name);
-    // ("resultsDiv").text("current temp" + response.temp + "current humidity" + response.main.humidity + "current wind speed" + response.main.windspeed + "U.V. index" + response.main.uv);
-    // ("fiveDayDiv").text("Five day forecast" + response.temp + response.main.humidity)
+    $("cityh1El").append(response.name)
+    $("resultsDiv").text(response.temp + respnse.main.humidity + response.wind.speed);
+    $("fiveDayDiv").text("Five day forecast" + response.temp + response.main.humidity)
 
-
-
-
-
+    prevEntryDiv.append.text(response.name)
 
 })
+var prevEntryDiv =
+    $("#search").on("click", function (event) {
+
+        $("<h1>").html(cityh1El).text(response.name);
+        $("<div>").appendchild(resultsDiv).text(response.temp + response.main.humidity + response.wind.speed);
+        $("<div>").appendchild(fiveDayDiv).text(response.temp + response.humidity);
+
+
+
+    })
