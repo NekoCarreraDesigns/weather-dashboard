d948703e10d3908207acc019f9d3bf02ad9fb39d
NekoCarreraDesigns <denvermaas@gmail.com>
2020-05-03 02:44:08 -0600

    finally got the current temp to work, working on the five day forecast, still need to get previous results to append

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-var APIKey = "a8873d0d448c9dc93f60cba6ef75cb3e";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=" + APIKey;
 
+var cityName
 var resultsDiv = $("<div>");
+var humidityDiv = $("<div>");
+var windDiv = $("<div>");
 var fiveDayDiv = $("<div>");
 var cityh1El = $("<h1>");
-var previousresultsDiv = $("<aside>");
+var prevEntryDiv = $("<aside>");
+
+
+function search(cityName) {
+    var APIKey = "&units=imperial&appid=a8873d0d448c9dc93f60cba6ef75cb3e";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q";
+
+
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + APIKey,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+        $(".cityh1El").html(response.name);
+        $(".resultsDiv").html("Temp:" + response.main.temp + "F");
+        $(".humidityDiv").html("Humidity:" + response.main.humidity);
+        $(".windDiv").html("Wind:" + response.wind.speed + "mph");
+        $("<h1>").append(cityh1El);
+        $("<div>").append(resultsDiv);
+        $("<div>").append(fiveDayDiv);
+        $("<div>").append(windDiv);
+        $("<div>").append(humidityDiv)
+
+
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + APIKey,
+            method: "GET",
+
+        }).then(function (response) {
+            console.log(queryURL)
+            console.log(response)
+
+            $(".fiveDayDiv").html(response.main.empty())
+            console.log(fiveDayDiv)
+            $("<div>").append(fiveDayDiv)
+
+
+        })
 
 
 
+    });
 
+}
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(queryURL)
-    console.log(response)
-    $("cityh1El").append(response.name)
-    $("resultsDiv").text(response.temp + respnse.main.humidity + response.wind.speed);
-    $("fiveDayDiv").text("Five day forecast" + response.temp + response.main.humidity)
-
-    prevEntryDiv.append.text(response.name)
-
+$("#search").on("click", function (event) {
+    console.log($(".search-bar").val())
+    var searchBar = $(".search-bar").val()
+    search(searchBar)
 })
-var prevEntryDiv =
-    $("#search").on("click", function (event) {
+var previousInput = [""];
+for (var i = 0; i < previousInput.length; i++) {
+    var oldInput = cityName + previousInput[i]
+    if (previousInput.length === oldInput) {
+        (".prevEntryDiv").html(previousInput[i])
+        $("<aside>").append(prevEntryDiv)
+        prevEntryDiv.localStorage.setItem(cityName, previousInput)
+    }
 
-        $("<h1>").html(cityh1El).text(response.name);
-        $("<div>").appendchild(resultsDiv).text(response.temp + response.main.humidity + response.wind.speed);
-        $("<div>").appendchild(fiveDayDiv).text(response.temp + response.humidity);
-
-
-
-    })
+}
